e7cb91a17616cfa9ca360b1afd0e7a658a4a26b4
Victor <victorliang@hotmail.com>
2023-01-05 22:42:18 -0500

    Formatting

--- day_12.py
+++ day_12.py
@@ -1,8 +1,10 @@
 INFINITY = 999
 
+
 def load_data():
     with open("input/day_12.txt") as f:
         return f.read().split('\n')
+
 
 def value_at(coordinate):
     """
@@ -21,56 +23,64 @@ def value_at(coordinate):
         value = 'z'
     return value
 
+
 def height_at(coordinate):
     return ord(value_at(coordinate))
+
 
 def find_coordinates(data, target_value):
     for y, row in enumerate(data):
         for x, value in enumerate(row):
             if value == target_value:
-                return (x,y)
+                return (x, y)
+
 
 def check_valid_moves(current_location, ascending=True):
-    (x,y) = current_location
-    up = (x, y+1)
-    down = (x, y-1)
-    left = (x-1, y)
-    right = (x+1, y)
+    (x, y) = current_location
+    up = (x, y + 1)
+    down = (x, y - 1)
+    left = (x - 1, y)
+    right = (x + 1, y)
 
     valid_moves = []
     for destination in [up, down, left, right]:
         if within_board(destination):
-            if ascending and height_at(destination) - height_at(current_location)<=1:
+            if ascending and height_at(destination) - height_at(current_location) <= 1:
                 valid_moves.append(destination)
-            elif not ascending and height_at(current_location) - height_at(destination) <=1:
+            elif not ascending and height_at(current_location) - height_at(destination) <= 1:
                 valid_moves.append(destination)
     return valid_moves
 
+
 def within_board(coordinate):
-    (x,y) = coordinate
+    (x, y) = coordinate
 
     max_x = len(data[0])
     max_y = len(data)
     return (0 <= x < max_x) and (0 <= y < max_y)
 
+
 def closeness_to_destination(coordinate):
-    (x,y) = coordinate
+    (x, y) = coordinate
     return hill_map[y][x].closeness_to_destination
 
+
 def node_at(coordinate):
-    (x,y) = coordinate
+    (x, y) = coordinate
     return hill_map[y][x]
+
 
 def reset_map(data):
     hill_map = []
     for y, row in enumerate(data):
-        map_row = [Node((x,y), target) for x, _ in enumerate(row)]
+        map_row = [Node((x, y), target) for x, _ in enumerate(row)]
         hill_map.append(map_row)
     return hill_map
 
+
 class Node:
     def __init__(self, coordinate, target):
-        (x,y) = coordinate
+        (x, y) = coordinate
         self.x = x
         self.y = y
         self.value = value_at(coordinate)
@@ -78,7 +88,6 @@ class Node:
         self.moves = INFINITY
         self.coming_from = None
         self.closeness_to_destination = self.calculate_closeness_to_destination(target)
-        
 
     def __repr__(self):
         return f"{self.height}"
@@ -94,12 +103,13 @@ class Node:
         delta_x = x_t - self.x
         delta_y = y_t - self.y
         delta_h = target_height - self.height
- 
+
         return abs(delta_x) + abs(delta_y) + abs(delta_h)
-    
+
     def set_moves(self, value, coming_from):
         self.moves = value
         self.coming_from = coming_from
+
 
 def visualize_height(hill_map):
     print("Height")
@@ -109,6 +119,7 @@ def visualize_height(hill_map):
         print('\n', end='')
     print("")
 
+
 def visualize_moves(hill_map):
     print("Minimum moves from the start (so far)")
     for row in hill_map:
@@ -117,6 +128,7 @@ def visualize_moves(hill_map):
         print('\n', end='')
     print('')
 
+
 def visualize_closeness_to_destination(hill_map):
     print("Closeness to destination (smaller is closer)")
     for row in hill_map:
@@ -124,6 +136,7 @@ def visualize_closeness_to_destination(hill_map):
             print(f'{node.closeness_to_destination: 3}', end='')
         print('\n', end='')
     print('')
+
 
 def visualize_path(hill_map, start, target):
     current_node = target
@@ -137,19 +150,18 @@ def visualize_path(hill_map, start, target):
             elif node_at(current_node).x - p_x == -1:
                 path_map[current_node] = '<'
             elif node_at(current_node).y - p_y == 1:
-                path_map[current_node] ='V'
+                path_map[current_node] = 'V'
             elif node_at(current_node).y - p_y == -1:
-                path_map[current_node] ='^'
+                path_map[current_node] = '^'
         current_node = prev
-
 
     print("Visualization of path:")
     for y, row in enumerate(hill_map):
         for x, _ in enumerate(row):
-            s = path_map.get((x,y), '-')
+            s = path_map.get((x, y), '-')
             print(s, end='')
         print('\n', end='')
-    print('')        
+    print('')
 
 
 def find_fewest_steps_to_target(start, target, ascending=True):
@@ -158,8 +170,8 @@ def find_fewest_steps_to_target(start, target, ascending=True):
     node_at(start).set_moves(0, None)
     current_node = start
     moves = 0
-    
-    while node_at(target).moves == INFINITY: 
+
+    while node_at(target).moves == INFINITY:
         moves += 1
         destination_nodes = []
         for current_node in nodes_to_check:
@@ -173,6 +185,7 @@ def find_fewest_steps_to_target(start, target, ascending=True):
         nodes_to_check = destination_nodes
 
     return node_at(target).moves
+
 
 def find_fewest_steps_to_target_height(start, target_height, ascending=True):
     nodes_to_check = [start]
@@ -195,7 +208,8 @@ def find_fewest_steps_to_target_height(start, target_height, ascending=True):
                         return node_at(coord).moves
                 destination_nodes.extend(valid_moves)
         nodes_to_check = destination_nodes
-    
+
+
 if __name__ == '__main__':
     data = load_data()
 
@@ -205,7 +219,7 @@ if __name__ == '__main__':
     hill_map = reset_map(data)
     steps_to_target = find_fewest_steps_to_target(start, target)
     visualize_path(hill_map, start, target)
-    
+
     print(f'This many moves to reach the target from {start}: {steps_to_target}')
 
     """Part 2: see if another starting point of height a is available
@@ -220,5 +234,5 @@ if __name__ == '__main__':
 
     hill_map = reset_map(data)
     steps_to_target = find_fewest_steps_to_target_height(start, target_height, ascending=False)
-   
+
     print(f'This many moves to reach height {target_height} target from {start}: {steps_to_target}')
--- day_13.py
+++ day_13.py
@@ -1,45 +1,49 @@
 from itertools import zip_longest
 from functools import cmp_to_key
 
+
 def load_data(file):
     with open(f"input/{file}.txt") as f:
         data = f.read().split('\n\n')
         data = [d.split('\n') for d in data]
         return data
 
+
 def parse_nested_list(string):
     result, _i = parse_list(string)
     return result
 
-def parse_list(string, i = 0):
+
+def parse_list(string, i=0):
     result = []
     first_parens = True
     current_number = ''
 
-    while i<len(string):
+    while i < len(string):
         char = string[i]
-        
+
         match char:
             case '[':
                 if first_parens:
                     first_parens = False
                     i += 1
-                else: 
+                else:
                     sublist, i = parse_list(string, i)
                     result.append(sublist)
             case ',':
                 if current_number:
                     result.append(int(current_number))
                 current_number = ''
-                i+=1
+                i += 1
             case ']':
                 if current_number:
                     result.append(int(current_number))
                 current_number = ''
-                return result, i+1 
+                return result, i + 1
             case number_char:
                 current_number += number_char
                 i += 1
+
 
 def check_order(left, right):
     for l, r in zip_longest(left, right):
@@ -57,7 +61,7 @@ def check_order(left, right):
 
         elif type(l) == int and type(r) == list:
             is_ordered = check_order([l], r)
-            
+
         elif l == None:
             return True
         elif r == None:
@@ -66,9 +70,11 @@ def check_order(left, right):
         if is_ordered != None:
             return is_ordered
 
+
 def comparison_function(left, right):
     mapping = {None: 0, True: 1, False: -1}
     return mapping[check_order(left, right)]
+
 
 if __name__ == "__main__":
     data = load_data("day_13")
@@ -80,19 +86,18 @@ if __name__ == "__main__":
     correct_indices = []
     for (i, (left, right)) in enumerate(pairs):
         index = i + 1
-        if result:= check_order(left, right):
+        if result := check_order(left, right):
             correct_indices.append(index)
 
     print(f'Part 1: sum of indices of correct pairs = {sum(correct_indices)}')
 
-   
-    all_signals = [first_divider:= [[2]], second_divider:= [[6]]]
+    all_signals = [first_divider := [[2]], second_divider := [[6]]]
     for pair in pairs:
         for signal in pair:
             all_signals.append(signal)
 
     sorted_signals = sorted(all_signals, key=cmp_to_key(comparison_function))
- 
+
     for i, signal in enumerate(sorted_signals):
         index = i + 1
         if signal == first_divider:
